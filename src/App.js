862d34fd20d2a44493128c02cadb86bcdd971b22
JWaldburger <49599965+JWaldburger@users.noreply.github.com>
2021-03-22 14:25:48 -0500

    Wow it's working !

--- src/App.js
+++ src/App.js
@@ -1,25 +1,236 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import
+  React
+  , { 
+      useEffect
+    , useReducer
+  } 
+from 'react';
+
+import { API } from 'aws-amplify';
+
+import { 
+  List
+  , Input
+  , Button 
+} from 'antd';
+
+import 'antd/dist/antd.css';
+
+import { listNotes } from './graphql/queries';
+
+import { v4 as uuid } from 'uuid';
+
+import {
+  createNote as CreateNote
+} from './graphql/mutations';
+
+const CLIENT_ID = uuid();
+
+const initialState = {
+  notes: []
+  , loading: true
+  , error: false
+  , form: {
+      name: ""
+    , description: ""
+  }
+};
+
+const reducer = (state, action) => {
+  switch(action.type) {
+    
+    case 'SET_NOTES':
+      return {
+        ...state
+        , notes: action.notes
+        , loading: false
+      };
+    
+    case 'ERROR':
+      return {
+        ...state
+        , error: true
+        , loading: true
+      };
+
+    case 'ADD_NOTE':
+      return {
+        ...state
+        , notes: [
+          action.note
+          , ...state.notes
+        ]
+      };
+
+    case 'RESET_FORM':
+      return {
+        ...state
+        , form: initialState.form
+      };
+      
+    case 'SET_INPUT':
+      return {
+        ...state
+        , form: {
+          ...state.form
+          , [action.name]: action.value
+        }
+      };
+
+    default:
+      return { 
+        ...state 
+      };
+  }
+};
+
+const App = () => {
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const fetchNotes = async () => {
+    try {
+
+      const notesData = await API.graphql({
+        query: listNotes
+      });
+
+      dispatch({
+        type: "SET_NOTES"
+        , notes: notesData.data.listNotes.items
+      });
+    }
+
+    catch (err) {
+      console.error(err);
+      dispatch({
+        type: "ERROR"
+      });
+    }
+  };
+
+  useEffect(
+    () => {
+      fetchNotes();
+    }
+    , []
+  );
+
+  const styles = {
+    container: {
+      padding: 20
+    }
+    , input: {
+      marginBottom: 10
+    }
+    , item: {
+      textAlign: 'left'
+    }
+    , p: {
+      color: '#1890ff'
+    }
+  };
+
+  const createNote = async () => {
+
+    // Destructuring...
+    const { form } = state;
+
+    // Lame form validation, but good enough...
+    if (!form.name || !form.description) {
+      return alert('please enter a name and description');
+    }
+    
+    const note = { 
+      ...form
+      , clientId: CLIENT_ID
+      , completed: false
+      , id: uuid() 
+    };
+
+    // Optimistic dispatch, updates local app state before calling GraphQL mutation endpoint...
+    dispatch({ 
+      type: 'ADD_NOTE'
+
+      // Shorthand syntax for note: note
+      , note
+    });
+
+    dispatch({ 
+      type: 'RESET_FORM' 
+    });
+
+
+    try {
+      await API.graphql({
+        query: CreateNote
+        , variables: { 
+          input: note 
+        }
+      });
+
+      console.log('successfully created note!');
+
+    } catch (err) {
+      console.error("error: ", err);
+    }
+  };
+
+  const onChange = (e) => {
+    dispatch({
+      type: "SET_INPUT"
+      , name: e.target.name
+      , value: e.target.value
+    });
+  };
+
+  const renderItem = (item) => {
+    return (
+      <List.Item
+        style={styles.item}
+      >
+        <List.Item.Meta 
+          title={item.name}
+          description={item.description}
+        />
+      </List.Item>
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={styles.container}
+    >
+      <Input
+        onChange={onChange}
+        value={state.form.name}
+        placeholder="Enter note name"
+        name='name'
+        style={styles.input}
+      />
+      <Input
+        onChange={onChange}
+        value={state.form.description}
+        placeholder="Enter description"
+        name='description'
+        style={styles.input}
+      />
+      <Button
+        onClick={createNote}
+        type="primary"
+      >
+        Create Note
+      </Button>
+      <List 
+        loading={state.loading}
+        dataSource={state.notes}
+        renderItem={renderItem}
+      />
     </div>
   );
-}
+};
 
 export default App;
